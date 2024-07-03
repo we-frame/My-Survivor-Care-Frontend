956c2928e890afdcb7b55b06fa88c2e088058ca7
SAMBIT MAJHI <majhisambit2@gmail.com>
2024-07-03 18:13:07 +0530

    Update LoginCard.tsx

--- components/Login/LoginCard.tsx
+++ components/Login/LoginCard.tsx
@@ -47,15 +47,15 @@ const LoginCard = ({ textCenter, data }: LoginCardTypes) => {
       //   },
       //   // bo÷y: JSON.stringify({ mode: "session" }), // using 'session' mode, but can also be 'cookie' or 'json'
       // });
-      // await fetch("https://api.mysurviour.agpro.co.in/auth/refresh", {
-      //   method: "POST",
-      //   credentials: "include", // this is required in order to send the refresh/session token cookie
-      //   headers: {
-      //     Accept: "application/json",
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify({ mode: "session" }), // using 'session' mode, but can also be 'cookie' or 'json'
-      // });
+      await fetch("https://api.mysurviour.agpro.co.in/auth/refresh", {
+        method: "POST",
+        credentials: "include", // this is required in order to send the refresh/session token cookie
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ mode: "session" }), // using 'session' mode, but can also be 'cookie' or 'json'
+      });
     } catch (error) {
       console.log(error);
     }
